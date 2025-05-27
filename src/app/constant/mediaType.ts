bf7e7a4042b6d2f8ebe321dbbeaac856422e9831
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-27 15:24:28 +0530

    Make constant of select fields

---
+++ src/app/constant/mediaType.ts
@@ -0,0 +1,24 @@
+const mediaValue = {
+  All: "",
+  TV: "tv",
+  Movie: "movie",
+  Ova: "ova",
+  Special: "special",
+  Ona: "ona",
+  Music: "music",
+  Cm: "cm",
+  Pv: "pv",
+  TvSpecial: "tv_special",
+};
+export const mediaType = [
+  { label: "All", value: mediaValue.All },
+  { label: "Tv", value: mediaValue.TV },
+  { label: "Movie", value: mediaValue.Movie },
+  { label: "Ova", value: mediaValue.Ova },
+  { label: "Special", value: mediaValue.Special },
+  { label: "Ona", value: mediaValue.Ona },
+  { label: "Music", value: mediaValue.Music },
+  { label: "Cm", value: mediaValue.Cm },
+  { label: "Pv", value: mediaValue.Pv },
+  { label: "Tv special", value: mediaValue.TvSpecial },
+];
---
+++ src/app/constant/statusType.ts
@@ -0,0 +1,13 @@
+const statusValue = {
+  All: "",
+  Airing: "airing",
+  Completed: "complete",
+  Upcoming: "upcoming",
+};
+
+export const statusType = [
+  { label: "All", value: statusValue.All },
+  { label: "Airing", value: statusValue.Airing },
+  { label: "Completed", value: statusValue.Completed },
+  { label: "Upcoming", value: statusValue.Upcoming },
+];
