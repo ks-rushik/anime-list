91421f3c9ea3f7d88fc7df68f5c699e3b45bf4a2
ks-rushik <rushi.katrodiya@kalkani.in>
2025-05-28 12:00:25 +0530

    Change import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import Provider from "@/app/component/Provider";
+import Provider from "@/app/component/QueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
